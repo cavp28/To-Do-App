c4994fa891d7ba20cd63376549f581d42627a1fd
Cesar Vargas <cesarvargas@Cesars-MacBook-Air.local>
2023-03-23 13:20:06 -0400

    Adding black format and comments to main.py in the backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from model import Todo
+
+# Methods to interact with MongoDB
 from database import (
     fetch_one_todo,
     fetch_all_todos,
@@ -25,20 +27,20 @@ app.add_middleware(
 )
 
 
-
 # Decorator to get the empty route
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
 
 
-
+# Get all To Dos
 @app.get("/api/todo")
 async def get_todo():
     response = await fetch_all_todos()
     return response
 
 
+# Get an specific To Do
 @app.get("/api/todo/{title}", response_model=Todo)
 async def get_todo_by_title(title):
     response = await fetch_one_todo(title)
@@ -47,6 +49,7 @@ async def get_todo_by_title(title):
     raise HTTPException(404, f"Not Todo item found with this title:  {title}")
 
 
+# Create a new To Do
 @app.post("/api/todo/", response_model=Todo)
 async def post_todo(todo: Todo):
     response = await create_todo(todo.dict())
@@ -55,6 +58,7 @@ async def post_todo(todo: Todo):
     raise HTTPException(400, "Something went wrong (bad request)")
 
 
+# Update a to do
 @app.put("/api/todo/{title}", response_model=Todo)
 async def put_todo(title: str, description: str):
     response = await update_todo(title, description)
@@ -63,6 +67,7 @@ async def put_todo(title: str, description: str):
     raise HTTPException(404, f"Not Todo item found with this title: {title}")
 
 
+# Delete a to do
 @app.delete("/api/todo/{title}")
 async def delete_todo(title):
     response = await remove_todo(title)
